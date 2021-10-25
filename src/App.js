87ebfadf07011552395da307fc71e0ad61fe0b8d
MarloPato <marlo_pato123@hotmail.com>
2021-10-25 16:16:31 +0200

    test 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Marlo was here
+          Edit <code>src/App.js</code> Marlo was there
         </p>
         <a
           className="App-link"
