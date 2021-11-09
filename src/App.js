1e646128774b22635331b2842011b5d1db195b23
MarloPato <marlo_pato123@hotmail.com>
2021-11-09 12:28:52 +0100

    deleted dead files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
     return (
         <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Editing <code>src/App.js</code> Matheus was here and marre is zmart ok
-                </p>
-                <a
-                    className='App-link'
-                    href='https://youtube.com'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
+            <h1>hello</h1>
         </div>
     )
 }
