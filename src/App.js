d5866091805e0cb7b42b85038ea3dbdd7b91ba2f
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-11-09 12:20:11 +0100

    added prettier file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editing <code>src/App.js</code> Matheus was here and marre is zmart ok
-        </p>
-        <a
-          className="App-link"
-          href="https://youtube.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                    Editing <code>src/App.js</code> Matheus was here and marre is zmart ok
+                </p>
+                <a
+                    className='App-link'
+                    href='https://youtube.com'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
 
 // wag1 on
