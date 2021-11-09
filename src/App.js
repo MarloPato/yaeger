8945cdbcbd2a7adfc9976a40e1ed64304c2d236c
MarloPato <marlo_pato123@hotmail.com>
2021-11-09 13:04:03 +0100

    Added canvas file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react"
+import Canvas from "./Canvas"
+
 function App() {
     return (
         <div className='App'>
             <h1>hello</h1>
+            <Canvas />
         </div>
     )
 }
