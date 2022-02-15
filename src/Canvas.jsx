49e02af7681e1af76a80fc40d61f624576ae9b5e
MarloPato <marlo_pato123@hotmail.com>
2022-02-15 13:14:15 +0100

    recent

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,34 +1,31 @@
-import React, { useRef, useEffect } from "react"
+import React, { useRef } from "react"
+import CanvasDraw, { getSaveData } from "react-canvas-draw"
 
-export default function Canvas(props) {
+export default function Canvas() {
     const canvasRef = useRef(null)
 
-    const draw = (ctx, frameCount) => {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-        ctx.fillStyle = "#000000"
-        ctx.beginPath()
-        ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
-        ctx.fill()
-    }
+    console.log("canvasRef")
+    console.log(canvasRef)
 
-    useEffect(() => {
-        const canvas = canvasRef.current
-        const context = canvas.getContext("2d")
-        let frameCount = 0
-        let animationFrameId
-
-        //Our draw came here
-        const render = () => {
-            frameCount++
-            draw(context, frameCount)
-            animationFrameId = window.requestAnimationFrame(render)
-        }
-        render()
-
-        return () => {
-            window.cancelAnimationFrame(animationFrameId)
-        }
-    }, [])
-
-    return <canvas ref={canvasRef} {...props} />
+    return (
+        <div className='wrapper'>
+            <div className='canvas'>
+                <CanvasDraw
+                    brushRadius='5'
+                    ref={(canvasDraw) => {
+                        canvasRef.current = canvasDraw
+                        console.log(canvasDraw)
+                    }}
+                />
+            </div>
+            <button
+                onClick={() => {
+                    console.log(canvasRef)
+                    console.log(canvasRef.current.getSaveData())
+                }}
+            >
+                Tryck mig
+            </button>
+        </div>
+    )
 }
