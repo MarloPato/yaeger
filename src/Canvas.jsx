0d03f78aac3aff40bbcb693d838cabe814e0255c
MarloPato <marlo_pato123@hotmail.com>
2022-03-07 14:00:56 +0100

    recent

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,14 +1,47 @@
-import React, { useRef } from "react"
+import React, { useRef, useState, useEffect } from "react"
 import CanvasDraw, { getSaveData } from "react-canvas-draw"
+import * as tf from "@tensorflow/tfjs"
 
 export default function Canvas() {
+    const [prediction, setPrediction] = useState(null)
+    const [linearModel, setLinearModel] = useState(null)
     const canvasRef = useRef(null)
 
     console.log("canvasRef")
     console.log(canvasRef)
 
+    async function train() {
+        // Define a model for linear regression.
+        const model = tf.sequential()
+        model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
+
+        // Prepare the model for training: Specify the loss and the optimizer.
+        model.compile({ loss: "meanSquaredError", optimizer: "sgd" })
+
+        // Training data, completely random stuff
+        const xs = tf.tensor1d([3.2, 4.4, 5.5])
+        const ys = tf.tensor1d([1.6, 2.7, 3.5])
+
+        // Train
+        await model.fit(xs, ys)
+        setLinearModel(model)
+
+        console.log("model trained!")
+    }
+
+    function predict(val) {
+        const output = linearModel.predict(tf.tensor2d([parseInt(val)], [1, 1]))
+        setPrediction(Array.from(output.dataSync())[0])
+    }
+
+    useEffect(() => {
+        train()
+    }, [])
+
     return (
         <div className='wrapper'>
+            <h1>predictions {prediction}</h1>
+            <input type='number' onChange={(e) => predict(e.target.value)} />
             <div className='canvas'>
                 <CanvasDraw
                     brushRadius='5'
